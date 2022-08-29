5194f2c55639bbe3db760259ea079c1ec47a117d
Freedman J Nicolas <100964607+KJNicolas@users.noreply.github.com>
2022-08-29 16:23:53 -0700

    Edited

---
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function MovieInformation() {
+  console.log('MovieInformation');
+
+  return (
+    <div>
+      money
+    </div>
+  );
+}
+
+export default MovieInformation;
